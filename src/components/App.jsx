de12558aa242eb406fda759e8c89dcf91ad78aa2
Nechai_Ihor <fredduke1983@gmail.com>
2023-04-11 10:07:39 +0300

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,12 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
     </div>
   );
 };
+
+//? --- https://api.themoviedb.org/3/movie/550?api_key=07952b117bf25f1a5db75594ad56755b
