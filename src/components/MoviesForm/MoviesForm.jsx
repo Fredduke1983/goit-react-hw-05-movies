a7387d3769df5e62c5c54974a4a9c4af62c1207f
Nechai_Ihor <fredduke1983@gmail.com>
2023-04-13 15:44:50 +0300

    up

--- src/components/MoviesForm/MoviesForm.jsx
+++ src/components/MoviesForm/MoviesForm.jsx
@@ -1,27 +1,36 @@
 import { Link, useSearchParams } from 'react-router-dom';
 import { MoviesFormStyle, MoviesList } from './MoviesForm.styled';
 import getSearch from 'utils/GetSearch';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import defaultImage from '../../img/no-symbol.png';
 
 export default function MoviesForm() {
   const [searchParams, setSearchParams] = useSearchParams({});
   const [searchList, setSearchList] = useState([]);
+  const query = searchParams.get('query');
 
-  function onSubmit(event) {
-    event.preventDefault();
-    const query = searchParams.get('query');
-
-    query &&
-      getSearch(query)
+  function getMovies(q) {
+    q &&
+      getSearch(q)
         .then(response => response)
         .then(movies => {
           setSearchList(movies.results);
         });
   }
 
+  function onSubmit(event) {
+    event.preventDefault();
+    setSearchParams({ query: event.target[0].value });
+    if (query === event.target[0].value) return;
+
+    getMovies(query);
+  }
+
+  useEffect(() => {
+    query !== '' && getMovies(query);
+  }, [query]);
+
   function onChange(event) {
-    setSearchParams({ query: event.target.value });
     if (!event.target.value) setSearchParams({});
   }
 
